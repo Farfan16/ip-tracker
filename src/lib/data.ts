a43ed257a3f96a54456c8f605cd270919df0d5bc
Farrel Fanny <farrelexamples@gmail.com>
2024-01-28 09:18:31 +0700

    rename folder from data to lib and add log if api response status not 200

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,13 +1,24 @@
+
 export const getIpAddress = async () => {
   const res = await fetch(`https://api.ipify.org?format=json`);
+  if (res.status != 200) {
+    console.log("Error fetch ip address ipify", Error);
+  }
   const ipData = await res.json();
   return ipData.ip;
 };
 
-export const fetchData = async ({ targetIpAddress }: { targetIpAddress: string }) => {
+export const fetchData = async ({
+  targetIpAddress,
+}: {
+  targetIpAddress: string;
+}) => {
   const response = await fetch(
     `https://geo.ipify.org/api/v2/country,city?apiKey=at_xYqpMZD1roLNOGOlBMiMQN6kMJmow&ipAddress=${targetIpAddress}`
   );
+  if (response.status != 200) {
+    console.log("Error fetch ipify IP Data", Error);
+  }
   const data = await response.json();
   const city = data.location.city;
   const country = data.location.country;
