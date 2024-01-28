03ba26811bf8897af03cb76c6999e61f9ddb4f67
Farrel Fanny <farrelexamples@gmail.com>
2024-01-28 10:02:55 +0700

    make responsive layout for mobile devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,10 +58,10 @@ export default function Home() {
   }, [ipAddress]);
 
   return (
-    <main className="flex h-screen flex-col items-center">
+    <main className="relative flex h-screen flex-col items-center">
       <div
         id="search-container"
-        className="relative z-10 min-w-full h-2/5 flex flex-col items-center justify-start p-10 gap-5 bg-fixed bg-pattern-desktop"
+        className="relative z-10 min-w-full h-2/5 flex flex-col items-center justify-start p-4 md:p-10 gap-5 bg-fixed bg-contain bg-pattern-mobile md:bg-pattern-desktop"
       >
         <h1 className="font-semibold text-white text-center text-2xl">
           IP Address Tracker
@@ -76,7 +76,7 @@ export default function Home() {
             name="ipInput"
             id="ipInput"
             placeholder="Search for any IP address or domain"
-            className="p-4 text-lg w-full text-[#2b2b2b] rounded-l-2xl min-w-[35rem] focus:outline-none"
+            className="p-4 text-base md:text-lg w-full text-[#2b2b2b] rounded-l-2xl md:min-w-[35rem] focus:outline-none placeholder:text-[0.8rem] md:placeholder:text-base"
           />
           <button
             type="submit"
@@ -85,10 +85,10 @@ export default function Home() {
             <Image src={arrowIcon} alt="arrow icon" width={14} height={14} />
           </button>
         </form>
-        <div className="absolute -bottom-[4.5rem] z-20 flex flex-row justify-start items-start divide-x max-w-[50rem] px-4 py-8 bg-white rounded-xl drop-shadow-xl">
+        <div className="absolute -bottom-[8.3rem] md:-bottom-[4.5rem] z-20 flex flex-col justify-center items-center md:flex-row md:justify-start md:items-start md:divide-x max-w-[50rem] gap-3 md:gap-0 px-16 py-4 md:px-4 md:py-8 bg-white rounded-xl drop-shadow-xl">
           <div
             id="ipAddress"
-            className="flex flex-col justify-start pr-8 gap-2 max-w-60"
+            className="flex flex-col items-center justify-center md:justify-start md:pr-8 md:gap-2 md:max-w-60"
           >
             <p className="uppercase font-semibold text-[#969696] text-xs">
               Ip Address
@@ -97,12 +97,12 @@ export default function Home() {
           </div>
           <div
             id="location"
-            className="flex flex-col justify-start  px-8 gap-2 max-w-60"
+            className="flex flex-col items-center justify-center md:justify-start md:px-8 md:gap-2 max-w-60"
           >
             <p className="uppercase font-semibold text-[#969696] text-xs">
               Location
             </p>
-            <p className="font-semibold text-[#2b2b2b] text-2xl">
+            <p className="font-semibold text-[#2b2b2b] text-xl md:text-2xl">
               {country && (
                 <>
                   {city}, {country}
@@ -112,23 +112,25 @@ export default function Home() {
           </div>
           <div
             id="timezone"
-            className="flex flex-col justify-start  px-8 gap-2 max-w-60"
+            className="flex flex-col items-center justify-center md:justify-start md:px-8 md:gap-2 max-w-60"
           >
             <p className="uppercase font-semibold text-[#969696] text-xs">
               Timezone
             </p>
-            <p className="font-semibold text-[#2b2b2b] text-2xl">
+            <p className="font-semibold text-[#2b2b2b] text-xl md:text-2xl">
               {timezone && <>UTC {timezone}</>}
             </p>
           </div>
           <div
             id="isp"
-            className="flex flex-col justify-start px-8 gap-2 max-w-60"
+            className="flex flex-col items-center justify-center md:justify-start md:px-8 md:gap-2 max-w-60"
           >
             <p className="uppercase font-semibold text-[#969696] text-xs">
               ISP
             </p>
-            <p className="font-semibold text-[#2b2b2b] text-2xl">{isp}</p>
+            <p className="font-semibold text-[#2b2b2b] text-xl md:text-2xl">
+              {isp}
+            </p>
           </div>
         </div>
       </div>
