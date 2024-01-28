394e3a1b6e476e57574565a84bd8061eca24a47e
Farrel Fanny <farrelexamples@gmail.com>
2024-01-28 10:02:28 +0700

    add mobile background

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,7 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
         "pattern-desktop": "url('../../public/images/pattern-bg-desktop.png')",
+        "pattern-mobile": "url('../../public/images/pattern-bg-mobile.png')",
       },
     },
   },
